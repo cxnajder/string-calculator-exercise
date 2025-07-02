7b02a92ff2f3a9a3bbefdefbbbcfdf801a6f7e6c
cxnajder <259448@student.pwr.edu.pl>
2025-07-02 08:27:51 +0200

    Add tests for case 1

--- src/test/java/StringCalculatorTestPackage/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTestPackage/StringCalculatorTest.java
@@ -1,11 +1,21 @@
 package StringCalculatorTestPackage;
 
+import StringCalculatorPackage.StringCalculator;
+
 import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class StringCalculatorTest {
-    @Test
-    public void TestTest(){
-        Assert.assertEquals(true, true);
+    @Test(priority=100)
+    public void EmptyString(){
+        Assert.assertEquals(StringCalculator.Add(""), 0);
+    }
+    @Test(priority=200)
+    public void OneNumber(){
+        Assert.assertEquals(StringCalculator.Add("1"), 1);
+    }
+    @Test(priority=300)
+    public void TwoNumbers(){
+        Assert.assertEquals(StringCalculator.Add("1,2"), 3);
     }
 }
