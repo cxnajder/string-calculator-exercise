e13b60efb423dc552544a8d44013c608c71a01d2
cxnajder <259448@student.pwr.edu.pl>
2025-07-02 08:21:51 +0200

    Add string calculator test class

---
+++ src/test/java/StringCalculatorTestPackage/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+package StringCalculatorTestPackage;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class StringCalculatorTest {
+    @Test
+    public void TestTest(){
+        Assert.assertEquals(true, true);
+    }
+}
