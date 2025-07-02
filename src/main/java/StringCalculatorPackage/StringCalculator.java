ad92541acdf78937598049cd54686428ba2e893f
cxnajder <259448@student.pwr.edu.pl>
2025-07-02 23:12:38 +0200

    Refactor calculator

--- src/main/java/StringCalculatorPackage/StringCalculator.java
+++ src/main/java/StringCalculatorPackage/StringCalculator.java
@@ -1,71 +1,76 @@
 package StringCalculatorPackage;
 
+import java.util.ArrayList;
+
 public class StringCalculator {
 
     static final String customDelimiterStartSign = "//";
     static final char customDelimiterEndSign = '\n';
-    public static int Add(String numbers) throws Exception {
-        if (numbers.isEmpty()){
+    public static int Add(String input) throws Exception {
+        if (input.isEmpty()){
             return 0;
         }
         String delimiter = ",";
-        String customDelimiter = checkCustomDelimiter(numbers);
+        String customDelimiter = checkCustomDelimiter(input);
         if (!customDelimiter.isEmpty()) {
             delimiter = customDelimiter;
-            numbers = removeCustomDelimiterPrefix(numbers);
+            input = removeCustomDelimiterPrefix(input);
         }
+        checkDelimiterAtTheEnd(input, delimiter);
+        input = replaceNewLineWithDelimiters(input, delimiter);
 
-        checkDelimiterAtTheEnd(numbers, delimiter);
-
-
-        numbers = replaceNewLineWithDelimiters(numbers, delimiter);
-
-        int indexOfCurrentNumber = 0;
-        int indexOfNextDelimiter = numbers.indexOf(delimiter);
         int sum = 0;
-        while (indexOfNextDelimiter > 0) {
-            sum += getIntFromNumbers(numbers, indexOfCurrentNumber, indexOfNextDelimiter, delimiter);
-            indexOfCurrentNumber = indexOfNextDelimiter + delimiter.length();
-            indexOfNextDelimiter = numbers.indexOf(delimiter, indexOfNextDelimiter + delimiter.length());
-        }
-        sum += getIntFromNumbers(numbers, indexOfCurrentNumber, numbers.length(), delimiter);
 
-        return sum;
-    }
-    private static Integer getIntFromNumbers(String numbers, int indexOfCurrentNumber, int indexOfNextDelimiter, String delimiter) throws Exception {
-        int number = 0;
-        try {
-            number = Integer.parseInt(numbers.substring(indexOfCurrentNumber, indexOfNextDelimiter));
-        } catch (NumberFormatException e) {
-            for (int i = indexOfCurrentNumber; i < numbers.length(); ++i){
-                if (!Character.isDigit(numbers.charAt(i)))
-                    throw new Exception(String.format("'%s' expected but '%c' found at position %d.", delimiter, numbers.charAt(i), i));
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        StringBuilder sb = new StringBuilder();
+        int currentIndex = 0;
+        while(currentIndex < input.length()) {
+            if(Character.isDigit(input.charAt(currentIndex))){
+                sb.append(input.charAt(currentIndex));
+                ++currentIndex;
+            }else{
+                nums.add(Integer.parseInt(sb.toString()));
+                sb = new StringBuilder();
+                int tempIndex = currentIndex;
+                while(!Character.isDigit(input.charAt(currentIndex))){
+                    sb.append(input.charAt(currentIndex));
+                    ++currentIndex;
+                }
+                if (!sb.toString().equals(delimiter)) {
+                    throw new Exception(String.format("'%s' expected but '%s' found at position %d.", delimiter, sb.toString(), tempIndex));
+                }
+                sb = new StringBuilder();
             }
         }
-        return number;
+        nums.add(Integer.parseInt(sb.toString()));
+
+        for (int n: nums){
+            sum += n;
+        }
+        return sum;
     }
 
-    private static void checkDelimiterAtTheEnd(String numbers, String delimiter) throws Exception {
-        String endString = numbers.substring(numbers.length()-delimiter.length());
+    private static void checkDelimiterAtTheEnd(String input, String delimiter) throws Exception {
+        String endString = input.substring(input.length()-delimiter.length());
         if (endString.equals(delimiter))
             throw new Exception("No delimiter at the end allowed.");
     }
 
-    private static String checkCustomDelimiter(String numbers){
-        if (numbers.length() < customDelimiterStartSign.length())
+    private static String checkCustomDelimiter(String input){
+        if (input.length() < customDelimiterStartSign.length())
             return "";
-        if (!numbers.startsWith(customDelimiterStartSign)){
+        if (!input.startsWith(customDelimiterStartSign)){
             return "";
         }
-        int indexOfCustomDelimiterEnd = numbers.indexOf(customDelimiterEndSign);
-        return numbers.substring(customDelimiterStartSign.length(), indexOfCustomDelimiterEnd);
+        int indexOfCustomDelimiterEnd = input.indexOf(customDelimiterEndSign);
+        return input.substring(customDelimiterStartSign.length(), indexOfCustomDelimiterEnd);
     }
 
-    private static String replaceNewLineWithDelimiters(String numbers, String delimeter){
-       return numbers.replace("\n", delimeter);
+    private static String replaceNewLineWithDelimiters(String input, String delimeter){
+       return input.replace("\n", delimeter);
     }
 
-    private static String removeCustomDelimiterPrefix(String numbers){
-        return numbers.substring(numbers.indexOf(customDelimiterEndSign) + 1);
+    private static String removeCustomDelimiterPrefix(String input){
+        return input.substring(input.indexOf(customDelimiterEndSign) + 1);
     }
 }
