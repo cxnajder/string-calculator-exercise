c3c2de114ff2e6c3ce849b632364e03e030891b4
cxnajder <259448@student.pwr.edu.pl>
2025-07-02 11:53:26 +0200

    Solve new line problem

--- src/main/java/StringCalculatorPackage/StringCalculator.java
+++ src/main/java/StringCalculatorPackage/StringCalculator.java
@@ -1,20 +1,34 @@
 package StringCalculatorPackage;
 
+import java.util.ArrayList;
+
 public class StringCalculator {
     public static int Add(String numbers){
         if (numbers.length() == 0){
             return 0;
         }
-
-        int index = numbers.indexOf(',');
+        String[] separators = {",", "\n"};
+        int index = indexOfNextNumber(numbers, separators);
         int sum = 0;
         while (index > 0) {
             sum += Integer.parseInt(numbers.substring(0, index));
             numbers = numbers.substring(index+1, numbers.length());
-            index = numbers.indexOf(',');
+            index = indexOfNextNumber(numbers, separators);
         }
         sum += Integer.parseInt(numbers);
 
         return sum;
+    }
+
+    private static int indexOfNextNumber(String numbers, String[] separators){
+        int index = -1;
+        for (String separator : separators) {
+            int foundIndex = numbers.indexOf(separator);
+            if (foundIndex >= 0) {
+                if ((index < 0)||(index > foundIndex))
+                    index = foundIndex;
+            }
+        }
+        return index;
     }
 }
