a4ff09b6eab1bff7905c026066c4c57a29cf0769
cxnajder <259448@student.pwr.edu.pl>
2025-07-02 19:09:05 +0200

    Add support for multi-char delimiter

--- src/main/java/StringCalculatorPackage/StringCalculator.java
+++ src/main/java/StringCalculatorPackage/StringCalculator.java
@@ -8,49 +8,49 @@ public class StringCalculator {
         if (numbers.isEmpty()){
             return 0;
         }
-        String[] delimiters = {",", "\n"};
+        String delimiter = ",";
 
         String customCustomDelimiter = checkCustomCustomDelimiter(numbers);
         if (!customCustomDelimiter.isEmpty()) {
-            delimiters[0] = customCustomDelimiter;
+            delimiter = customCustomDelimiter;
             numbers = numbers.substring(numbers.indexOf(customDelimiterEndSign)+1);
         }
 
-        if (delimiterAtTheEnd(numbers, delimiters))
+        if (delimiterAtTheEnd(numbers, delimiter))
             throw new Exception("No delimiter at the end allowed.");
 
-        int indexOfNextNumber = getIndexOfNextNumber(numbers, delimiters);
+        numbers = replaceNewLineWithDelimiter(numbers, delimiter);
+
+        int indexOfNextDelimiter = getIndexOfNextDelimiter(numbers, delimiter);
         int sum = 0;
-        while (indexOfNextNumber > 0) {
-            sum += Integer.parseInt(numbers.substring(0, indexOfNextNumber));
-            numbers = numbers.substring(indexOfNextNumber+1);
-            indexOfNextNumber = getIndexOfNextNumber(numbers, delimiters);
+        while (indexOfNextDelimiter > 0) {
+            sum += Integer.parseInt(numbers.substring(0, indexOfNextDelimiter));
+            numbers = numbers.substring(indexOfNextDelimiter + delimiter.length());
+            indexOfNextDelimiter = getIndexOfNextDelimiter(numbers, delimiter);
         }
         sum += Integer.parseInt(numbers);
 
         return sum;
     }
 
-    private static int getIndexOfNextNumber(String numbers, String[] delimiters){
+    private static int getIndexOfNextDelimiter(String numbers, String delimiter){
         int index = -1;
-        for (String delimiter : delimiters) {
-            int foundIndex = numbers.indexOf(delimiter);
-            if (foundIndex >= 0) {
-                if ((index < 0)||(index > foundIndex))
-                    index = foundIndex;
-            }
+        int foundIndex = numbers.indexOf(delimiter);
+        if (foundIndex >= 0) {
+            if ((index < 0)||(index > foundIndex))
+                index = foundIndex;
         }
+
         return index;
     }
 
-    private static boolean delimiterAtTheEnd(String numbers, String[] delimiters) {
+    private static boolean delimiterAtTheEnd(String numbers, String delimiter) {
         String endString = "";
-        for (String delimiter : delimiters) {
-            if (endString.length() != delimiter.length())
-                endString = numbers.substring(numbers.length()-delimiter.length());
-            if (endString.equals(delimiter))
-                return true;
-        }
+        if (endString.length() != delimiter.length())
+            endString = numbers.substring(numbers.length()-delimiter.length());
+        if (endString.equals(delimiter))
+            return true;
+
         return false;
     }
 
@@ -62,5 +62,9 @@ public class StringCalculator {
         }
         int indexOfCustomDelimiterEnd = numbers.indexOf(customDelimiterEndSign);
         return numbers.substring(customDelimiterStartSign.length(), indexOfCustomDelimiterEnd);
+    }
+
+    private static String replaceNewLineWithDelimiter(String numbers, String delimeter){
+       return numbers.replace("\n", delimeter);
     }
 }
