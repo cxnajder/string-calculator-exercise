a154786655be370dca4a77cd315d8984f3a85031
cxnajder <259448@student.pwr.edu.pl>
2025-07-02 18:09:15 +0200

    Add tests for case 5 - custom delimiter

--- src/test/java/StringCalculatorTestPackage/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTestPackage/StringCalculatorTest.java
@@ -81,4 +81,42 @@ public class StringCalculatorTest {
         Exception except = Assert.expectThrows(Exception.class, () -> StringCalculator.Add("1,2,"));
         Assert.assertEquals(except.getMessage(), "No separator at the end allowed");
     }
+
+    @Test(priority=800)
+    public void handleDifferentDelimiter1(){
+        try {
+            Assert.assertEquals(StringCalculator.Add("//;\\n1;3"), 4);
+        } catch (Exception e) {
+            Assert.fail();
+        }
+    }
+
+    @Test(priority=820)
+    public void handleDifferentDelimiter2(){
+        try {
+            Assert.assertEquals(StringCalculator.Add("//|\\n1|2|3"), 6);
+        } catch (Exception e) {
+            Assert.fail();
+        }
+    }
+
+    @Test(priority=840)
+    public void handleDifferentDelimiter3(){
+        try {
+            Assert.assertEquals(StringCalculator.Add("//sep\\n2sep5"), 7);
+        } catch (Exception e) {
+            Assert.fail();
+        }
+    }
+
+    @Test(priority=860)
+    public void handleDifferentDelimiterError(){
+        Exception except = Assert.expectThrows(Exception.class, () -> StringCalculator.Add("//|\n1|2,3"));
+    }
+
+    @Test(priority=870)
+    public void handleDifferentDelimiterErrorWithMessage(){
+        Exception exception = Assert.expectThrows(Exception.class, () -> StringCalculator.Add("//|\n1|2,3"));
+        Assert.assertEquals(exception.getMessage(), "'|' expected but ',' found at position 3.");
+    }
 }
