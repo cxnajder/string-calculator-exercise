4b0aade4f8f9e6b5d9df135bd0fc54a0ea9722e3
cxnajder <259448@student.pwr.edu.pl>
2025-07-02 08:30:38 +0200

    Pass empty string test

--- src/main/java/StringCalculatorPackage/StringCalculator.java
+++ src/main/java/StringCalculatorPackage/StringCalculator.java
@@ -2,6 +2,9 @@ package StringCalculatorPackage;
 
 public class StringCalculator {
     public static int Add(String numbers){
+        if (numbers.length() == 0){
+            return 0;
+        }
         return -1;
     }
 }
